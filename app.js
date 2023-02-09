5a6a1ab4d888839aaa1df6bba474b06e64367988
José Igor <89313216+igusil@users.noreply.github.com>
2023-02-09 14:47:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const url =
-  'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=';
+  'https://pt.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=';
 
 const formDOM = document.querySelector('.form');
 const inputDOM = document.querySelector('.form-input');
